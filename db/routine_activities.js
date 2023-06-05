94b88e8feca685ddebd296da20f65790b3e0e1da
Blizzrard <stang05gtsil@gmail.com>
2023-06-04 18:29:38 -0600

    All db tests passed

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -18,15 +18,90 @@ async function addActivityToRoutine({
   } catch (error) {}
 }
 
-async function getRoutineActivityById(id) {}
+async function getRoutineActivityById(id) {
+  try {
+    const response = await client.query(
+      `
+      SELECT * FROM routine_activities WHERE id = ($1)
+    `,
+      [id]
+    );
+    return response.rows[0];
+  } catch (error) {}
+}
 
-async function getRoutineActivitiesByRoutine({ id }) {}
+async function getRoutineActivitiesByRoutine({ id }) {
+  try {
+    const response = await client.query(
+      `
+      SELECT a.id, a.name, a.description FROM routine_activities ra
+      JOIN activities a ON a.id=ra."activityId"
+      WHERE ra."routineId" = ($1);
+    `,
+      [id]
+    );
+    return response.rows;
+  } catch (error) {}
+}
 
-async function updateRoutineActivity({ id, ...fields }) {}
+async function updateRoutineActivity({ id, ...fields }) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
 
-async function destroyRoutineActivity(id) {}
+  if (setString.length === 0) {
+    return;
+  }
 
-async function canEditRoutineActivity(routineActivityId, userId) {}
+  try {
+    const {
+      rows: [routineActivities],
+    } = await client.query(
+      `
+      UPDATE routine_activities
+      SET ${setString}
+      WHERE id=${id}
+      RETURNING *;
+    `,
+      Object.values(fields)
+    );
+
+    return routineActivities;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function destroyRoutineActivity(id) {
+  try {
+    const response = await client.query(
+      `
+      DELETE FROM routine_activities WHERE id = ($1)
+      RETURNING *;
+    `,
+      [id]
+    );
+    return response.rows[0];
+  } catch (error) {}
+}
+
+async function canEditRoutineActivity(routineActivityId, userId) {
+  try {
+    const response = await client.query(
+      `
+      SELECT r."creatorId" FROM routine_activities ra
+      JOIN routines r ON r.id=ra."routineId"
+      WHERE ra.id = ($1);
+    `,
+      [routineActivityId]
+    );
+    if (response.rows[0].creatorId === userId) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {}
+}
 
 module.exports = {
   getRoutineActivityById,
--- db/routines.js
+++ db/routines.js
@@ -47,27 +47,193 @@ async function getAllRoutines() {
         routineInfo.rows[i].activities.split("%");
       routineInfo.rows[i].activities = await routineInfo.rows[i].activities.map(
         (key) => {
-          console.log(key);
           return JSON.parse(key);
         }
       );
     }
-    // console.log(routineInfo.rows[0].activities[0]);
     return routineInfo.rows;
   } catch (error) {}
 }
 
-async function getAllPublicRoutines() {}
+async function getAllPublicRoutines() {
+  try {
+    const routineInfo = await client.query(`
+    SELECT DISTINCT
+    r.name AS name,
+    u.username AS "creatorName",
+    r.id,
+    r."isPublic", 
+    r.goal,
+    STRING_AGG(CONCAT('{', '"id": ', a.id, ', ', '"name": "', a.name, '", ', '"count": ', ra.count, ', ', '"duration": ', ra.duration, ', '
+    '"description": "', a.description, '", ', '"routineId": ', ra."routineId", ', ', '"routineActivityId": ', ra.id, '}'), '%') AS activities,
+    r."creatorId"
+    FROM routines r 
+    JOIN routine_activities ra ON r.id=ra."routineId"
+     JOIN activities a ON a.id=ra."activityId"
+      JOIN users u ON u.id=r."creatorId"
+      WHERE "isPublic" = true
+       GROUP BY 1,2,3;
+    `);
+    for (let i = 0; i < routineInfo.rows.length; i++) {
+      routineInfo.rows[i].activities =
+        routineInfo.rows[i].activities.split("%");
+      routineInfo.rows[i].activities = await routineInfo.rows[i].activities.map(
+        (key) => {
+          return JSON.parse(key);
+        }
+      );
+    }
+    return routineInfo.rows;
+  } catch (error) {}
+}
 
-async function getAllRoutinesByUser({ username }) {}
+async function getAllRoutinesByUser({ username }) {
+  try {
+    const routineInfo = await client.query(
+      `
+  SELECT DISTINCT
+  r.name AS name,
+  u.username AS "creatorName",
+  r.id,
+  r."isPublic", 
+  r.goal,
+  STRING_AGG(CONCAT('{', '"id": ', a.id, ', ', '"name": "', a.name, '", ', '"count": ', ra.count, ', ', '"duration": ', ra.duration, ', '
+  '"description": "', a.description, '", ', '"routineId": ', ra."routineId", ', ', '"routineActivityId": ', ra.id, '}'), '%') AS activities,
+  r."creatorId"
+  FROM routines r 
+  JOIN routine_activities ra ON r.id=ra."routineId"
+   JOIN activities a ON a.id=ra."activityId"
+    JOIN users u ON u.id=r."creatorId"
+    WHERE u.username = ($1)
+     GROUP BY 1,2,3;
+  `,
+      [username]
+    );
+    for (let i = 0; i < routineInfo.rows.length; i++) {
+      routineInfo.rows[i].activities =
+        routineInfo.rows[i].activities.split("%");
+      routineInfo.rows[i].activities = await routineInfo.rows[i].activities.map(
+        (key) => {
+          return JSON.parse(key);
+        }
+      );
+    }
+    return routineInfo.rows;
+  } catch (error) {}
+}
 
-async function getPublicRoutinesByUser({ username }) {}
+async function getPublicRoutinesByUser({ username }) {
+  try {
+    const routineInfo = await client.query(
+      `
+SELECT DISTINCT
+r.name AS name,
+u.username AS "creatorName",
+r.id,
+r."isPublic", 
+r.goal,
+STRING_AGG(CONCAT('{', '"id": ', a.id, ', ', '"name": "', a.name, '", ', '"count": ', ra.count, ', ', '"duration": ', ra.duration, ', '
+'"description": "', a.description, '", ', '"routineId": ', ra."routineId", ', ', '"routineActivityId": ', ra.id, '}'), '%') AS activities,
+r."creatorId"
+FROM routines r 
+JOIN routine_activities ra ON r.id=ra."routineId"
+ JOIN activities a ON a.id=ra."activityId"
+  JOIN users u ON u.id=r."creatorId"
+  WHERE u.username = ($1) AND "isPublic" = true
+   GROUP BY 1,2,3;
+`,
+      [username]
+    );
+    for (let i = 0; i < routineInfo.rows.length; i++) {
+      routineInfo.rows[i].activities =
+        routineInfo.rows[i].activities.split("%");
+      routineInfo.rows[i].activities = await routineInfo.rows[i].activities.map(
+        (key) => {
+          return JSON.parse(key);
+        }
+      );
+    }
+    return routineInfo.rows;
+  } catch (error) {}
+}
 
-async function getPublicRoutinesByActivity({ id }) {}
+async function getPublicRoutinesByActivity({ id }) {
+  try {
+    const routineInfo = await client.query(
+      `
+SELECT DISTINCT
+r.name AS name,
+u.username AS "creatorName",
+r.id,
+r."isPublic", 
+r.goal,
+STRING_AGG(CONCAT('{', '"id": ', a.id, ', ', '"name": "', a.name, '", ', '"count": ', ra.count, ', ', '"duration": ', ra.duration, ', '
+'"description": "', a.description, '", ', '"routineId": ', ra."routineId", ', ', '"routineActivityId": ', ra.id, '}'), '%') AS activities,
+r."creatorId"
+FROM routines r 
+JOIN routine_activities ra ON r.id=ra."routineId"
+JOIN activities a ON a.id=ra."activityId"
+JOIN users u ON u.id=r."creatorId"
+WHERE a.id = ($1) AND "isPublic" = true
+ GROUP BY 1,2,3;
+`,
+      [id]
+    );
+    for (let i = 0; i < routineInfo.rows.length; i++) {
+      routineInfo.rows[i].activities =
+        routineInfo.rows[i].activities.split("%");
+      routineInfo.rows[i].activities = await routineInfo.rows[i].activities.map(
+        (key) => {
+          return JSON.parse(key);
+        }
+      );
+    }
+    return routineInfo.rows;
+  } catch (error) {}
+}
 
-async function updateRoutine({ id, ...fields }) {}
+async function updateRoutine({ id, ...fields }) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
 
-async function destroyRoutine(id) {}
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [routines],
+    } = await client.query(
+      `
+      UPDATE routines
+      SET ${setString}
+      WHERE id=${id}
+      RETURNING *;
+    `,
+      Object.values(fields)
+    );
+
+    return routines;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function destroyRoutine(id) {
+  try {
+    const deletedRoutine = await client.query(`
+    DELETE FROM routine_activities WHERE "routineId" = ($1);
+    `,
+      [id]
+    );
+    const deletedRoutineActivity = await client.query(`
+    DELETE FROM routines WHERE id = ($1);
+    `,
+      [id]
+    );
+  } catch (error) {}
+}
 
 module.exports = {
   getRoutineById,
